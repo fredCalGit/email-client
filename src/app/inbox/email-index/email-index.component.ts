b6d0f46134b1c542820180823069cdde022faff9
Frederico Muinhos <fred.cal@hotmail.com>
2022-12-26 15:41:12 -0500

    implemented email summary list

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EmailService } from '../email.service';
 
+interface EmailSummary {
+  id: string;
+  from: string;
+  subject: string;
+}
 @Component({
   selector: 'app-email-index',
   templateUrl: './email-index.component.html',
   styleUrls: ['./email-index.component.css']
 })
 export class EmailIndexComponent {
+  @Input() emails: EmailSummary[]
+  constructor(private emailService: EmailService) { }
 
+  ngOnInit() {
+    this.emailService.getEmails().subscribe((emails) => this.emails = emails)
+  }
 }
